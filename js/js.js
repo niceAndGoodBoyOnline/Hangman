7deeaec3448695eb01004fef5ba9e21b4cbbf434
nice and good boy online <felix.ruttan@gmail.com>
2019-12-01 23:49:45 -0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,28 +1,32 @@
+/*
+These are global variables used in more than one function.
+*/
+
 let moreGame = document.getElementById('moreGame');
 let hangmanZone = document.getElementById('hangmanZone');
 let winZone = document.getElementById('winZone');
+let hintZone = document.getElementById('hintZone');
 let buttonDiv = document.getElementById("buttonDiv");
 let resetButton = document.getElementById('restartButton');
+let topLine = document.getElementById('topH1')
+let hintButton = document.getElementById('hintButton');
 
 
 let enableButtons = true;
 let life = 7;
+let score = 0;
+let scoreMulti = 1;
+let hintMulti = 2;
 let word = '';
+let hangmanList = populateList()
 
-
-////////////////////////////////////////
-////////Words///////////////////////////
-////////////////////////////////////////
-
-let BUTT = {word:'BUTT', hint:'we all got em'};
-let NICE = {word:'NICE', hint:'When Amir sees your code'};
-let COMMITTEE = {word: 'COMMITTEE', hint:'a group of people appointed for a specific function, typically consisting of members of a larger group.'}
-let hangmanList = [BUTT, NICE, COMMITTEE];
+console.log(hangmanList)
 
 
 prepareGame();
 resetButton.onclick = function() {location.reload()};
 moreGame.onclick = prepareGame;
+hintButton.onclick = openNav;
 
 
 ////////////////////////////////////////
@@ -32,33 +36,31 @@ moreGame.onclick = prepareGame;
 
 function prepareGame(){
 	
-	if (enableButtons == true){
-		instantiateButtons();
-	}
-
-
-	life = 7;
-	
-	
+		
 	if (hangmanList.length > 0){
 	randomNum = Math.floor(Math.random() * hangmanList.length)
 	}
 	else{
 		winZone.innerHTML = "You've done got all the word";
 		buttonDiv.innerHTML = ''
-		enableButtons = true;
 		moreGame.style.display = 'none';
+		score = 0;
 		return
 	}
 	
-	console.log(randomNum);
+	instantiateButtons();
+	life = 7;
+	hintMulti = 2;
+		
 	hangmanZone.innerHTML = ''
 	winZone.innerHTML = ''
 	preparedWord = prepareTheWord(hangmanList[randomNum]);
+	scoreMulti = preparedWord.length
 	hangmanList.splice(randomNum, 1)
 	hideLetters(preparedWord);
 	moreGame.style.display = 'none';
 	word = preparedWord;
+	topLine.innerHTML = "HANGMAN HP: " + life;
 }
 
 
@@ -78,20 +80,24 @@ function checkWord(pickedLetter){
 		prepWin();
 	}
 	
-	manageLife(foundLetter)
+	destroyButton(pickedLetter)
+	manageLife(foundLetter, pickedLetter)
 }
 
 
-function manageLife(foundLetter){
+function manageLife(foundLetter, pickedLetter){
+	
 	if (foundLetter == false){
 		life--;
+		topLine.innerHTML = "HANGMAN HP: " + life;
 	}
+	
 	
 	if (life < 1){
 		winZone.innerHTML = 'YOU LOSE M8'
 		buttonDiv.innerHTML = ''
 		moreGame.style.display = 'block';
-		enableButtons = true;
+		score = 0;
 	}
 }
 
@@ -106,9 +112,10 @@ function revealLetter(thisIndex){
 
 // function prepare a win screen, and enable a new game button
 function prepWin(){
-	enableButtons = false;
+	buttonDiv.innerHTML = ''
 	moreGame.style.display = 'block';
-	winZone.innerHTML += "nice, u are win now"
+	score += life*scoreMulti*hintMulti;
+	winZone.innerHTML += "Good Job! Your Score:" + score;
 }
 
 
@@ -116,6 +123,24 @@ function prepWin(){
 ////////Game Setup//////////////////////
 ////////////////////////////////////////
 
+
+function populateList(){
+	let localList = []
+	
+	localList.push(
+		CODE = {word:'CODE', hint:'A system used for brevity or secrecy of communication, in which arbitrarily chosen words, letters, or symbols are assigned definite meanings.'},
+		NICE = {word:'NICE', hint:'When Amir sees your code'},
+		COMMITTEE = {word: 'COMMITTEE', hint:'A group of people appointed for a specific function, typically consisting of members of a larger group.'},
+		TATTOO = {word: 'TATTOO', hint:'A form of body modification where a design is made by inserting ink'},
+		ELECTRICITY = {word: 'ELECTRICITY', hint: 'Is the set of physical phenomena associated with the presence and motion of electric charge.'},
+		DICTIONARY = {word: 'DICTIONARY', hint: 'A book, optical disc, mobile device, or online lexical resource containing a selection of the words of a language, giving information about their meanings,'},
+		ARRAY = {word: 'ARRAY', hint:'To place in a proper or desired order'},
+		INSTANTIATE = {word: 'INSTANTIATE', hint:'To provide and instance of or concrete evidence in support of a theory, concept, claim etc.'},
+		LEXICON = {word: 'LEXICON', hint:'The vocabulary of a particular language, field, social class, person etc.'},
+		CHICKEN = {word: 'CHICKEN', hint:'A type of domesticated fowl, primarily used as a food source for humans and occasionally kept as pets'}
+		)
+	return localList
+}
 
 function hideLetters(prepedWord){
 	let hangmanZone = document.getElementById('hangmanZone');
@@ -125,10 +150,9 @@ function hideLetters(prepedWord){
 }
 
 
-
 function prepareTheWord(choosenWord){
-	
 	wordAsList = Array.from(choosenWord.word);
+	hintZone.innerHTML = choosenWord.hint
 	return wordAsList
 }
 
@@ -140,14 +164,29 @@ function instantiateButtons(){
 	}
 }
 
+
+function destroyButton(buttonLabel){
+	document.getElementById(buttonLabel + "Button").style.display = 'none';	
+}
+
+
 function MakeButton(buttonLabel){
 	let input = document.createElement('input');
 	input.classList.add('letterBtn');
-	input.ID = buttonLabel + "Button"
+	input.id = buttonLabel + "Button"
 	input.value = buttonLabel;
 	input.type = 'button';
 	input.width = '4vw';
 	input.height = '2vh';
 	input.addEventListener("click", function() {checkWord(input.value)});
 	buttonDiv.appendChild(input);
+}
+
+function openNav() {
+  document.getElementById("hintSN").style.width = "100%";
+  hintMulti = 1;
+}
+
+function closeNav() {
+  document.getElementById("hintSN").style.width = "0";
 }
